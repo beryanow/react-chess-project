6bbe88bc01838791681f780d7f9346a60aab378b
Maxim Beryanov <berjnov.ru@mail.ru>
2020-03-27 15:19:30 +0700

    Create Empty Board

---
+++ src/components/board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import '../index.css';
+import Square from './square.js';
+
+export default class Board extends React.Component {
+    renderSquare(i, squareShade) {
+        return <Square style={this.props.squares[i] ? this.props.squares[i].style : null}
+                       shade={squareShade}/>
+    }
+
+    render() {
+        const board = [];
+        for (let i = 0; i < 8; i++) {
+            const squareRow = [];
+            for (let j = 0; j < 8; j++) {
+                squareRow.push(this.renderSquare((i * 8) + j, ((i + j) % 2) ? "dark-square" : "light-square"));
+            }
+            board.push(<div className="board-row"> {squareRow} </div>)
+        }
+
+        return (
+            <div>
+                {board}
+            </div>
+        );
+    }
+}
---
+++ src/components/square.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import '../index.css';
+
+export default function Square(props) {
+    return <button className={"square " + props.shade}
+                   style={props.style}/>
+}
