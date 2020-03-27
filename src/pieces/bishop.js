0224f75b2ffdaf642460f3b5b97961727dfadad1
Maxim Beryanov <berjnov.ru@mail.ru>
2020-03-27 16:16:13 +0700

    Edit Icons

--- src/pieces/bishop.js
+++ src/pieces/bishop.js
@@ -2,6 +2,6 @@ import Piece from './piece.js';
 
 export default class Bishop extends Piece {
     constructor(player) {
-        super(player, (player === 1 ? "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg"));
+        super(player, (player === 1 ? "/images/bishop_white.png" : "/images/bishop_black.png"));
     }
 }
--- src/pieces/queen.js
+++ src/pieces/queen.js
@@ -2,6 +2,6 @@ import Piece from './piece.js';
 
 export default class Queen extends Piece {
     constructor(player) {
-        super(player, (player === 1 ? "/images/queen_white.png" : "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg"));
+        super(player, (player === 1 ? "/images/queen_white.png" : "/images/queen_black.png"));
     }
 }
