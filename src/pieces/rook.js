864700c5d623822fe9c22b683dc1a43c229ff319
Maxim Beryanov <berjnov.ru@mail.ru>
2020-03-28 20:29:44 +0700

    Make Rook Move

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -4,4 +4,38 @@ export default class Rook extends Piece {
     constructor(player) {
         super(player, (player === 1 ? "/piece_images/rook_white.png" : "/piece_images/rook_black.png"));
     }
+
+    isMovePossible(previous, forward) {
+        return (Math.abs(previous - forward) % 8 === 0) ||  // vertical line check
+            ((forward >= (previous - previous % 8)) && (forward <= previous + (7 - previous % 8))); // horizontal line check
+    }
+
+    getStepwisePath(previous, forward) {
+        let stepwisePath = [];
+        let start, end, increment;
+
+        if (previous > forward) {
+            start = forward;
+            end = previous;
+        } else {
+            start = previous;
+            end = forward;
+        }
+
+        if (Math.abs(previous - forward) % 8 === 0) {
+            // vertical path
+            increment = 8;
+            start += 8;
+        } else {
+            // horizontal path
+            increment = 1;
+            start += 1;
+        }
+
+        for (let i = start; i <= end; i += increment) {
+            stepwisePath.push(i);
+        }
+
+        return stepwisePath;
+    }
 }
